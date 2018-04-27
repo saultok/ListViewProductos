1f15f637c143df198f96f7d36c26e82cd4105298
saultok <37222664+saultok@users.noreply.github.com>
2018-04-27 12:13:30 -0500

    Practica 9

--- app/src/main/java/com/example/fundacioncuervo/listviewproductos/MainActivity.java
+++ app/src/main/java/com/example/fundacioncuervo/listviewproductos/MainActivity.java
@@ -1,42 +1,103 @@
 package com.example.fundacioncuervo.listviewproductos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private TextView txt;
     private ListView lista;
 
-    private String productos[] = {"Computadora", "Mouse", "Dulces", "Hojas",
+    /*private String productos[] = {"Computadora", "Mouse", "Dulces", "Hojas",
             "Lapices", "Lentes","Reloj", "Cuchara", "Celular", "Mesa", "Refrigerador",
             "Horno", "Audifonos"};
 
     private String categoria[] = {"Electronica","Electronica","Dulceria","Papeleria","Papeleria","Moda","Perfumeria",
-            "Hogar", "Electronicos", "Hogar", "Electrodomesticos", "Electrodomesticos", "Electronica"};
-    @Override
+            "Hogar", "Electronicos", "Hogar", "Electrodomesticos", "Electrodomesticos", "Electronica"};*/
 
+    private List<String>lproductos = new ArrayList<>();
+    private List<String>lcategoria = new ArrayList<>();
+
+    private void actualizarTabla(){
+        String productos[] = new String [lproductos.size()];
+        lproductos.toArray(productos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, productos);
+        lista.setAdapter(adapter);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long
+                    id) {
+                txt.setText("Categoria elegido: " + lcategoria.get(position));
+            }
+        });
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
             txt = (TextView) findViewById(R.id.textView);
             lista = (ListView) findViewById(R.id.lista);
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                    android.R.layout.simple_list_item_1,productos);
-            lista.setAdapter(adapter);
+              actualizarTabla();
+
+    }
 
 
-            lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long
-                        id) {
-                    txt.setText("Categoria elegido: " + categoria[position]);
-                }
-            });
-    }}
+    public void llamaVentana(View w){
+        Log.e("Verifica Click","Click");
+        Intent i = new Intent(this,AgregarActivity.class);
+        startActivityForResult(i,1234);
+    }
+
+
+    @Override
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+       /* String cad = data.getStringExtra("Producto");
+        Toast.makeText(this,cad,Toast.LENGTH_LONG).show();
+        String c = data.getStringExtra("Categoria");
+        Toast.makeText(this,c,Toast.LENGTH_LONG).show();*/
+       lproductos.add(data.getStringExtra("Producto"));
+       lcategoria.add(data.getStringExtra("Categoria"));
+        actualizarTabla();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater i = getMenuInflater();
+        i.inflate(R.menu.menu_principal,menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.op_salir:
+                Toast.makeText(this,"saliendo",Toast.LENGTH_SHORT).show();
+                break;
+                case  R.id.op_info:
+                    Toast.makeText(this,"Informacion",Toast.LENGTH_SHORT).show();
+                    break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
 
